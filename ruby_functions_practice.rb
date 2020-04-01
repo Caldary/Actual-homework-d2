1b00326c4ad5084be39c4e7d17dd671b6ec007ab
Paulo Rocha <paulorocha1988@gmail.com>
2020-04-01 09:11:34 +0100

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,36 @@
+# def return_10()
+ #   return 10
+#end
+
+
+# def add(number_1, number_2)
+#  return number_1 + number_2
+#end
+
+
+#def subtract(number_1, number_2)
+#   total = number_1 - number_2
+#    return total
+#end
+
+
+
+# def multiply(number_1, number_2)
+#     return number_1 * number_2
+# end
+
+
+
+
+# def divide(number_1, number_2)
+#     return number_1 / number_2
+# end
+
+
+
+
+
+def test_string(string_1)
+    length_of_string = string_1.length
+    return string_1 + length_of_string
+end
